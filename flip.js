297237e7457b185ad331c27a88cdafa20d589bd8
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-05-22 14:24:40 -0400

    Add flip.js

---
+++ flip.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const filenames = fs.readdirSync("./data-bak/followers");
+
+for (filename of filenames) {
+  const userId = filename.replace(/.txt$/, "");
+  const followedUserIds = fs
+    .readFileSync(`./data-bak/followers/${filename}`, "utf8")
+    .trim()
+    .split("\n");
+  for (followedUserId of followedUserIds) {
+    fs.writeFileSync(`./data/channels/${followedUserId}.txt`, userId, {
+      flag: "a",
+    });
+  }
+  console.log(`Wrote user IDs for ${userId}`);
+}
